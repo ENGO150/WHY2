30c18b73abe856318efd991fe757f4a540272966
ENGO150 <v.smejkal06@gmail.com>
2022-03-07 17:33:51 +0100

    created decryption function

---
+++ src/decrypter.c
@@ -0,0 +1,12 @@
+#include "../include/decrypter.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *decryptText(char *text, char *key)
+{
+    printf("fuk of\n");
+
+    exit(0);
+    return NULL;
+}
