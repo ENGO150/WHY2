77ca16e21581d888e023b77bc61449d3dce06636
Šebestíček <44745014+SebestikCZ@users.noreply.github.com>
2022-03-20 16:15:42 +0100

    Delete include directory

--- include/decrypter.h
+++
@@ -1,6 +0,0 @@
-#ifndef WHY2_DECRYPTER_H
-#define WHY2_DECRYPTER_H
-
-char *decryptText(char *text, char *key); //TEXT from WILL BE DECRYPTED WITH KEY AND RETURNED
-
-#endif
--- include/encrypter.h
+++
@@ -1,6 +0,0 @@
-#ifndef WHY2_ENCRYPTER_H
-#define WHY2_ENCRYPTER_H
-
-char *encryptText(char *text, char *keyNew); //TEXT from WILL BE ENCRYPTED WITH RANDOM PASSWORD [KEY] (WHICH WILL BE PRINTED OUT) AND RETURNED
-
-#endif
