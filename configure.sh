4772e7ae6f432a92dc44fa888b4d68afb0b2329f
ENGO150 <v.smejkal06@gmail.com>
2022-04-27 18:23:05 +0200

    simplified install cmd

--- configure.sh
+++ configure.sh
@@ -5,9 +5,9 @@ DISTRO=$(lsb_release -is)
 
 # Get COMMAND
 if [[ $DISTRO == "Arch" ]]; then
-    COMMAND="sudo pacman -S --needed json-c && sudo pacman -S --needed curl"
+    COMMAND="sudo pacman -S --needed json-c curl"
 elif [[ $DISTRO == "Ubuntu" ]] || [[ $DISTRO == "Debian" ]]; then
-    COMMAND="sudo apt install libjson-c3 && sudo apt install curl"
+    COMMAND="sudo apt install libjson-c3 curl"
 else
     # 'Unsupported' distro
     echo "It seems you are using unsupported distribution... Don't worry, just install https://github.com/json-c/json-c (+ CURL if you haven't installed it already) and you'll be fine."
