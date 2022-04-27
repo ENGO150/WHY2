1325d6ac9a6c18897443f2983e7675f39883b475
ENGO150 <v.smejkal06@gmail.com>
2022-04-27 18:51:33 +0200

    switched version to v2.1

--- include/flags.h
+++ include/flags.h
@@ -7,7 +7,7 @@
 
 #define INVALID_KEY 1
 
-#define VERSION "v2.0"
+#define VERSION "v2.1"
 #define VERSIONS_URL "https://raw.githubusercontent.com/ENGO150/WHY2/master/versions.json"
 #define VERSIONS_NAME "versions.json"
 
