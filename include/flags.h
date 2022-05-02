c5dca2c782547799f563310f90d888230b8f4453
ENGO150 <v.smejkal06@gmail.com>
2022-05-02 18:01:12 +0200

    changed SKIP_CHECK to variable & created getter and setter

--- include/flags.h
+++ include/flags.h
@@ -1,16 +1,22 @@
 #ifndef WHY2_FLAGS_H
 #define WHY2_FLAGS_H
 
+//CONSTS
 #define KEY_LENGTH 50
 #define ENCRYPTION_SEPARATOR '.'
 #define ENCRYPTION_SEPARATOR_STRING "."
 
 #define INVALID_KEY 1
 
-#define VERSION "v3.0"
+#define VERSION "v2.2"
 #define VERSIONS_URL "https://raw.githubusercontent.com/ENGO150/WHY2/master/versions.json"
 #define VERSIONS_NAME "versions.json"
 
-#define SKIP_CHECK 1
+//VARIABLES
+static int skipCheck = 0;
+
+//FUNCTIONS
+int getSkipCheck();
+void setSkipCheck(int skipCheckNew);
 
 #endif
---
+++ src/flags.c
@@ -0,0 +1,11 @@
+#include <why2/flags.h>
+
+int getSkipCheck()
+{
+    return skipCheck;
+}
+
+void setSkipCheck(int skipCheckNew)
+{
+    skipCheck = skipCheckNew;
+}
