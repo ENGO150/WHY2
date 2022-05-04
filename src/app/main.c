32440a0cb7cc1f14fa393d7e9b719036cabf13b5
ENGO150 <v.smejkal06@gmail.com>
2022-05-04 18:58:51 +0200

    created basic info code for why2-app

--- src/app/main.c
+++ src/app/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
+#include <why2/encrypter.h>
+#include <why2/flags.h>
+
+#define TEXT_TO_ENCRYPT "Some text yk"
+
 int main(void)
 {
-    printf("Hello, World!\n");
-    
+    setNoOutput(1);
+
+    printf
+    (
+        "Hi.\n"
+        "This is an simple application written using WHY2 Encryption System.\n\n"
+
+        "\"%s\" => \"%s\"\n\n"
+
+        "If you'd like to know more about WHY2 Encryption System, please visit: https://github.com/ENGO150/WHY2/wiki \n"
+        "Thank you so much for supporting this project!\n"
+
+        , TEXT_TO_ENCRYPT, encryptText(TEXT_TO_ENCRYPT, NULL)
+    );
+
     return 0;
 }
