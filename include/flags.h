cd9f29041d1e81a869057f5054165d7da521e52d
ENGO150 <v.smejkal06@gmail.com>
2022-05-04 19:27:51 +0200

    created comments

--- include/flags.h
+++ include/flags.h
@@ -2,20 +2,20 @@
 #define WHY2_FLAGS_H
 
 //CONSTS
-#define ENCRYPTION_SEPARATOR '.'
-#define ENCRYPTION_SEPARATOR_STRING "."
+#define ENCRYPTION_SEPARATOR '.' //SEPARATOR BETWEEN KEYS
+#define ENCRYPTION_SEPARATOR_STRING "." //SAME AS ENCRYPTION_SEPARATOR BUT AS STRING
 
-#define INVALID_KEY 1
+#define INVALID_KEY 1 //EXIT VALUE FOR INVALID KEY
 
-#define VERSION "v3.1"
-#define VERSIONS_URL "https://raw.githubusercontent.com/ENGO150/WHY2/stable/versions.json"
-#define VERSIONS_NAME "versions.json"
+#define VERSION "v3.1" //VERSION OF CURRENT BUILD     > DO NOT TOUCH THIS <
+#define VERSIONS_URL "https://raw.githubusercontent.com/ENGO150/WHY2/stable/versions.json" //URL FOR GETTING versions.json
+#define VERSIONS_NAME "versions.json" //do I have to explain this?
 
 //VARIABLES
-static int keyLength = 50;
+static int keyLength = 50; //LENGTH OF KEY     > DO NOT TOUCH THIS <
 
-static int skipCheck = 0;
-static int noOutput = 0;
+static int skipCheck = 0; //BOOLEAN FOR SKIPPING VERSION CHECK
+static int noOutput = 0; //BOOLEAN FOR NOT PRINTING OUTPUT WHEN ENCRYPTING/DECRYPTING
 
 //GETTERS
 int getSkipCheck();
