1303d3d22009cc25383b9b682697911ef89a457b
ENGO150 <v.smejkal06@gmail.com>
2022-05-25 17:44:53 +0200

    added missing comments to why2-test

--- src/lib/test/main.c
+++ src/lib/test/main.c
@@ -6,21 +6,25 @@
 
 int main(void)
 {
+    //VARIABLES
     char *buffer = malloc(64);
     int exitCode = 0;
 
+    //FLAGS
     inputFlags flags =
     {
         0, //SKIP CHECK
         0, //NO OUTPUT
     };
 
+    //ENCRYPT & DECRYPT
     outputFlags encrypted = encryptText(TEST_TEXT, NULL, flags);
 
     strcpy(buffer, encrypted.outputText); //GET ENCRYPTED TEXT
 
     encrypted = decryptText(encrypted.outputText, encrypted.usedKey, flags);
 
+    //COMPARE DIFFERENCE
     if (strcmp(encrypted.outputText, TEST_TEXT) == 0)
     {
         printf("Test successful!\n\nTEXT: %s\nOUTPUT: %s\nKEY: %s\n", TEST_TEXT, buffer, encrypted.usedKey);
