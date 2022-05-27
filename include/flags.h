1406de2f37dfe117540e860dcc6b031aef6d40a3
ENGO150 <v.smejkal06@gmail.com>
2022-05-27 17:26:20 +0200

    added comment to previous commit

--- include/flags.h
+++ include/flags.h
@@ -39,7 +39,7 @@ static int keyLength = 50; //LENGTH OF KEY     > DO NOT TOUCH THIS <
 
 //GETTERS
 int getKeyLength();
-inputFlags noFlags();
+inputFlags noFlags(); //THIS GENERATES inputFlags WITH DEFAULT VALUES
 
 //SETTERS
 void setKeyLength(int keyLengthNew);
