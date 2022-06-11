98ec31c9cf3b4533cf08e63046e4d3f18d663656
ENGO150 <v.smejkal06@gmail.com>
2022-06-11 16:59:45 +0200

    removed the 'git' files
    omfg I'm so stupid -> you can do this MUCH easier

--- include/git.h
+++
@@ -1,8 +0,0 @@
-#ifndef WHY2_GIT_H
-#define WHY2_GIT_H
-
-#include <git2.h>
-
-int cloneRepository(git_repository *repo, int argc, char **argv); // CLONES GIT REPOSITORY
-
-#endif
--- include/why2.h
+++ include/why2.h
@@ -10,7 +10,6 @@
 #include <why2/decrypter.h>
 #include <why2/encrypter.h>
 #include <why2/flags.h>
-#include <why2/git.h>
 #include <why2/misc.h>
 
 #endif
