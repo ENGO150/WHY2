5d53a6e706ce9ca9035451c080602c4051df0066
ENGO150 <v.smejkal06@gmail.com>
2022-08-24 14:53:02 +0200

    created countRepeatedKeySize function
    and guess what is it for
    
    hehe

--- include/misc.h
+++ include/misc.h
@@ -12,6 +12,7 @@ _Bool checkKey(char *key); //CHECKS IF KEY IS VALID
 _Bool checkText(char *text); //CHECKS IF TEXT IS VALID
 unsigned long countIntLength(int number); //RETURNS LENGTH OF number
 unsigned long countUnusedKeySize(char *text, char *key); //COUNT unusedKeySize
+unsigned long countRepeatedKeySize(char *text, char *key); //COUNT repeatedKeySize
 unsigned long compareTimeMicro(struct timeval startTime, struct timeval finishTime); //COMPARE TIMES IN MICROSECONDS
 
 #endif
