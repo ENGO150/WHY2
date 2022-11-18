312d68c74147f4396ca44952a5212af4bd3e3184
ENGO150 <v.smejkal06@gmail.com>
2022-11-18 19:46:57 +0100

    created EXIT_CODES and INVALID_FILE code

--- include/logger/flags.h
+++ include/logger/flags.h
@@ -2,6 +2,11 @@
 #define WHY2_LOGGER_FLAGS_H
 
 //CONSTS
+const enum EXIT_CODES //exit codes you fucking idiot (2#)
+{
+    INVALID_FILE = -1 //THIS WILL HAPPEN IF YOU USE TOO MUCH LOGS lol
+};
+
 #define LOG_FORMAT "yyyy-mm-dd_xxx.log" //THE LAST xxx IS FOR BASE-16 NUMBER OF USAGE THAT DAY (SO MAX IS 4095 [FFF] USAGES PER DAY)
 
 #endif
