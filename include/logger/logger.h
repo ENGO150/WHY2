3a456bf63a637f5d9fa33913cdc81de1cf4cae0b
ENGO150 <v.smejkal06@gmail.com>
2022-11-22 18:23:25 +0100

    created writeLog function

--- include/logger/logger.h
+++ include/logger/logger.h
@@ -2,5 +2,6 @@
 #define WHY2_LOGGER_LOGGER_H
 
 int initLogger(char *directoryPath); //CREATES LOGGING FILE IN directoryPath
+void writeLog(int loggerFile, char *logMessage); //WRITES logMessage TO loggerFile
 
 #endif
