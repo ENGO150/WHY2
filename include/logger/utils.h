7e65ac5550c3b330e309671b0a57045b33a2eb23
ENGO150 <v.smejkal06@gmail.com>
2022-11-22 18:15:13 +0100

    created deallocateLogger function in why2-utils header

--- include/logger/utils.h
+++ include/logger/utils.h
@@ -1,6 +1,6 @@
 #ifndef WHY2_LOGGER_UTILS_C
 #define WHY2_LOGGER_UTILS_C
 
-
+void deallocateLogger(int logger); //USE THIS IF YOU WANT TO DEALLOCATE FILE POINTER RETURNED BY logger'S initLogger
 
 #endif
