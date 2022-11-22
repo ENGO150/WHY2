90935724fc98ab10c927721c6404db69c04e8c1e
ENGO150 <v.smejkal06@gmail.com>
2022-11-22 18:35:46 +0100

    created WRITE_FORMAT flag

--- include/logger/flags.h
+++ include/logger/flags.h
@@ -9,6 +9,9 @@ const enum RETURN_CODES //exit codes you fucking idiot (2#)
 
 #define LOG_FORMAT "yyyy-mm-dd_xxx.log" //THE LAST xxx IS FOR BASE-16 NUMBER OF USAGE THAT DAY (SO MAX IS 4095 [FFF] USAGES PER DAY)
 #define LOG_FORMATTING "%s/%04d-%02d-%02d_%03x" //SAME THING AS LOG_FORMAT, BUT USED IN sprintf IN logger.c, NOT AS LENGTH
+
+#define WRITE_FORMAT "[hh:mm:ss] " //LOG MESSAGE'S PREFIX (THE SPACE AT THE END IS INTENTIONAL)
+
 #define MAX_USAGE 0xfff //LOOK AT LOG_FORMAT
 
 #endif
