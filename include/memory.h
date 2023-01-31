114695f65366cfc491e56d74308708fa80fdb9f0
ENGO150 <v.smejkal06@gmail.com>
2023-01-31 16:09:26 +0100

    removed every file-related function declaration from memory

--- include/memory.h
+++ include/memory.h
@@ -1,15 +1,9 @@
 #ifndef WHY2_MEMORY_H
 #define WHY2_MEMORY_H
 
-#include <stdio.h> //TODO: Make the FILE* shit smarter | idk if it is possible
-
 void *why2_malloc(unsigned long size);
 void *why2_calloc(unsigned long element, unsigned long size);
 void *why2_realloc(void *pointer, unsigned long size);
-
-FILE *why2_fopen(char *file_name, char *modes);
-FILE *why2_fdopen(int file, char *modes);
-int why2_open(char *file, int flags, ...);
 
 void why2_free(void *pointer);
 
