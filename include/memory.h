1285f52b6737093f802163b3dc313253ea96f5bc
ENGO150 <v.smejkal06@gmail.com>
2023-02-05 18:23:03 +0100

    declared why2_fopen, why2_fdopen, why2_open and why2_opendir

--- include/memory.h
+++ include/memory.h
@@ -7,6 +7,11 @@ void *why2_realloc(void *pointer, unsigned long size);
 
 char *why2_strdup(char *string);
 
+void *why2_fopen(char *name, char *modes);
+void *why2_fdopen(int file, char *modes);
+int why2_open(char *name, int flags, ...);
+void *why2_opendir(char *name);
+
 void why2_free(void *pointer);
 
 void why2_clean_memory(char *identifier); //identifier SPECIFIES WHICH NODES TO DEALLOCATE | THIS IS BASICALLY GARBAGE COLLECTOR | PASS why2_get_default_memory_identifier() FOR DEALLOCATING EVERYTHING
