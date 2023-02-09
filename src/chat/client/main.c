6348fab4d9f67dd262e54ceae0ca92bc6795936a
ENGO150 <v.smejkal06@gmail.com>
2023-02-09 09:09:26 +0100

    created new why2-chat module

---
+++ src/chat/client/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Here will be client...\n");
+
+    return 0;
+}
---
+++ src/chat/server/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Here will be server...\n");
+
+    return 0;
+}
