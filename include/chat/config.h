d25ca16f4e090f1720a6fb6af15f2693e2e93b60
ENGO150 <v.smejkal06@gmail.com>
2023-05-02 18:54:37 +0200

    created WHY2_CHAT_CODE_PICK_USERNAME macro
    these will be sent on some events etc.

--- include/chat/config.h
+++ include/chat/config.h
@@ -19,6 +19,9 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #ifndef WHY2_CHAT_CONFIG_H
 #define WHY2_CHAT_CONFIG_H
 
+//MACROS
+#define WHY2_CHAT_CODE_PICK_USERNAME "code_001"
+
 void why2_chat_init_config(void); //CHECK IF CONFIG EXISTS, CREATE IT
 
 #endif
