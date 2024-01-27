73d0f64fc992b347674c1925042fb1f9c634b919
ENGO150 <v.smejkal06@gmail.com>
2024-01-27 22:19:38 +0100

    implemented WHY2_MIN_USERNAME_LENGTH

--- src/chat/misc.c
+++ src/chat/misc.c
@@ -403,7 +403,7 @@ void *why2_communicate_thread(void *arg)
 
             decoded_buffer = get_string_from_json_string(raw, "message"); //DECODE
 
-            invalid_username = (strlen(decoded_buffer) > WHY2_MAX_USERNAME_LENGTH) || !check_username(decoded_buffer); //CHECK FOR USERNAMES LONGER THAN 20 CHARACTERS
+            invalid_username = (strlen(decoded_buffer) > WHY2_MAX_USERNAME_LENGTH) || (strlen(decoded_buffer) < WHY2_MIN_USERNAME_LENGTH) || (!check_username(decoded_buffer)); //CHECK FOR USERNAMES LONGER THAN 20 CHARACTERS
 
             why2_deallocate(username);
             username = decoded_buffer;
@@ -604,7 +604,7 @@ void *why2_listen_server(void *socket)
                 exiting = 1; //EXIT THIS THREAD
             } else if (strcmp(read + 8, WHY2_CHAT_CODE_PICK_USERNAME) == 0) //PICK USERNAME
             {
-                printf("%sEnter username (a-Z, 0-9; max %d characters):\n", WHY2_CLEAR_AND_GO_UP, WHY2_MAX_USERNAME_LENGTH);
+                printf("%sEnter username (a-Z, 0-9; %d-%d characters):\n", WHY2_CLEAR_AND_GO_UP, WHY2_MAX_USERNAME_LENGTH, WHY2_MAX_USERNAME_LENGTH);
                 fflush(stdout);
 
                 goto continue_input;
