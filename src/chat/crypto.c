df4f7aae7dbce50fc9d761dc8701e6897870af91
ENGO150 <v.smejkal06@gmail.com>
2024-02-23 10:24:38 +0100

    created generate_prime fn
    it uses gmp 😎

--- src/chat/crypto.c
+++ src/chat/crypto.c
@@ -17,3 +17,30 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <why2/chat/crypto.h>
+
+#include <stdio.h>
+#include <sys/random.h>
+
+#include <gmp.h>
+
+//LOCAL
+void generate_prime(mpz_t x)
+{
+    //RANDOM
+    gmp_randstate_t state;
+    gmp_randinit_default(state);
+    unsigned long random_buffer; //SEED
+
+    do
+    {
+        if (getrandom(&random_buffer, sizeof(unsigned long), GRND_NONBLOCK) == -1) why2_die("getrandom fn failed!");
+
+        //GENERATE RANDOM PRIME USING random_buffer SEED
+        gmp_randseed_ui(state, random_buffer);
+        mpz_urandomb(x, state, WHY2_CHAT_KEY_BITS);
+        mpz_nextprime(x, x);
+    } while (mpz_probab_prime_p(x, WHY2_CHAT_PRIME_ITERS) == 0); //CHECK FOR PRIME PROBABILITY
+
+    //DEALLOCATION
+    gmp_randclear(state);
+}
