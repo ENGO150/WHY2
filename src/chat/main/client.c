4134f9d1b376ccd55c82688bb9a3110c1bd4cd5c
ENGO150 <v.smejkal06@gmail.com>
2024-04-27 11:49:00 +0200

    implemented LIST command in client
    not in server yet haha

--- src/chat/main/client.c
+++ src/chat/main/client.c
@@ -180,6 +180,9 @@ int main(void)
                 );
 
                 fflush(stdout);
+            } else if (command(line, WHY2_CHAT_COMMAND_LIST, &cmd_arg)) //HELP CMD
+            {
+                why2_send_socket(WHY2_CHAT_CODE_LIST, NULL, listen_socket);
             }
         } else
         {
