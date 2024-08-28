b92ed3d6a8e5c224adbc51df5b7c9f8fa3ab00bd
ENGO150 <v.smejkal06@gmail.com>
2024-08-28 11:48:52 +0200

    declared why2_get_version

--- include/misc.h
+++ include/misc.h
@@ -30,6 +30,7 @@ extern "C" {
 void why2_generate_text_key_chain(char *key, int *text_key_chain, int text_key_chain_size); //GENERATES ARRAY FOR ENCRYPTION/DECRYPTION
 char *why2_generate_key(int key_length); //GENERATE ENCRYPTION KEY
 void why2_deallocate_output(why2_output_flags flags); //DEALLOCATES flags
+char *why2_get_version(void); //GET VERSION OF WHY2
 enum WHY2_EXIT_CODES why2_check_version(void); //THIS FUNCTION CHECKS IF LATEST WHY2_VERSION OF WHY2 IS USED
 enum WHY2_EXIT_CODES why2_check_key(char *key); //CHECKS IF KEY IS VALID
 enum WHY2_EXIT_CODES why2_check_text(char *text); //CHECKS IF TEXT IS VALID
