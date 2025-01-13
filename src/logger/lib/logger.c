917b466b11f57f30917a90bfa16def384c9c2d33
ENGO150 <v.smejkal06@gmail.com>
2025-01-13 19:22:53 +0100

    replacing \n with space in logger
    instead of null-term and breaking the string

--- src/logger/lib/logger.c
+++ src/logger/lib/logger.c
@@ -121,7 +121,7 @@ void why2_write_log(int loggerFile, char *logMessage)
     char *logMessageUsed = why2_strdup(logMessage);
     for (int i = 0; i < (int) strlen(logMessageUsed); i++)
     {
-        if (logMessageUsed[i] == '\n') logMessageUsed[i] = '\0';
+        if (logMessageUsed[i] == '\n') logMessageUsed[i] = ' ';
     }
 
     //VARIABLES
