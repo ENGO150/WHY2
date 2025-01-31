2768280e7f8b0d69f0bc6fc43660b4fdc66a8914
ENGO150 <v.smejkal06@gmail.com>
2025-01-31 15:52:00 +0100

    declared why2_chat_init_authority

--- include/chat/config.h
+++ include/chat/config.h
@@ -34,6 +34,8 @@ extern "C" {
 void why2_chat_init_server_config(void); //CHECK IF SERVER CONFIG EXISTS, CREATE IT
 void why2_chat_init_client_config(void); //Dementia is a term used to describe a group of symptoms affecting memory, thinking and social abilities. In people who have dementia, the symptoms interfere with their daily lives. Dementia isn't one specific disease. Several diseases can cause dementia. ...
 
+void why2_chat_init_authority(void); //CREATE AUTHORITY DIRECTORY
+
 char *why2_toml_read(const char* path, const char* key); //READ key FROM path TOML FILE
 void why2_toml_write(const char *path, const char *key, const char *value); //WRITE value AS key INTO path TOML FILE
 why2_bool why2_toml_contains(const char *path, const char *key); //CHECK IF path CONTAINS key
