9ab4f50fc6e22827aee39f8e4ada2c9c90d6bf12
ENGO150 <v.smejkal06@gmail.com>
2025-01-31 14:01:41 +0100

    sending WHY2_CHAT_CODE_LIST as a code

--- src/chat/main/client.c
+++ src/chat/main/client.c
@@ -251,7 +251,7 @@ int main(void)
                 why2_deallocate(final_message);
             } else if (command(line, WHY2_CHAT_COMMAND_LIST, &cmd_arg)) //LIST CMD
             {
-                why2_send_socket(WHY2_CHAT_CODE_LIST, NULL, listen_socket);
+                why2_send_socket_code(NULL, NULL, listen_socket, WHY2_CHAT_CODE_LIST);
             } else if (command(line, WHY2_CHAT_COMMAND_VERSION, &cmd_arg)) //VERSION CMD
             {
                 why2_send_socket(WHY2_CHAT_CODE_VERSION, NULL, listen_socket);
