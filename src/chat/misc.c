4d64d73c07bb2391d044bdd1ebf53eb7018f4178
ENGO150 <v.smejkal06@gmail.com>
2025-02-01 20:57:07 +0100

    waiting for WHY2_CHAT_CODE_USERNAME

--- src/chat/misc.c
+++ src/chat/misc.c
@@ -554,11 +554,24 @@ void *why2_communicate_thread(void *arg)
                 goto deallocation;
             }
 
-            if ((raw = read_user(connection, &raw_ptr)) == NULL) //READ
+            //KEEP READING UNTIL CODE ARRIVES
+            char *code = NULL;
+            why2_bool exiting_read = 0;
+            do
             {
-                force_exiting = 1; //FAILURE
-                goto deallocation;
-            }
+                //DEALLOCATE
+                why2_deallocate(code);
+
+                if ((raw = read_user(connection, &raw_ptr)) == NULL) //READ
+                {
+                    force_exiting = 1; //FAILURE
+                    goto deallocation;
+                }
+
+                //COMPARE CODE
+                code = get_string_from_json_string(raw, "code");
+                exiting_read = code != NULL && strcmp(code, WHY2_CHAT_CODE_USERNAME) == 0;
+            } while (!exiting_read);
 
             decoded_buffer = get_string_from_json_string(raw, "message"); //DECODE
 
