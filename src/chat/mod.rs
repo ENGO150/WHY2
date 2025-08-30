1c487104178ac1cf02d4dad3f3447836a495346c
ENGO150 <v.smejkal06@gmail.com>
2025-08-30 19:44:30 +0200

    create chat modules
    and client & server binary

--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -15,6 +15,3 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
-pub mod core;
-pub mod chat;
