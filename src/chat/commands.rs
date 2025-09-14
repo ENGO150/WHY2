52496043af5101a22a442a98ffb29f9eaf41e380
ENGO150 <engo@satan.red>
2025-09-14 14:40:45 +0200

    create commands source file

--- src/chat/commands.rs
+++ src/chat/commands.rs
@@ -15,9 +15,3 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
-pub mod commands;
-pub mod config;
-pub mod crypto;
-pub mod network;
-pub mod options;
