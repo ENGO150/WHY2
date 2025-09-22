5bccd126835df7638dc2016eb20c3b1772b7ef42
ENGO150 <engo@satan.red>
2025-09-22 17:41:03 +0200

    implement empty method for RexData

--- src/core/rex/misc.rs
+++ src/core/rex/misc.rs
@@ -16,6 +16,18 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-//pub mod encrypter;
-pub mod misc;
-pub mod options;
+use crate::core::rex::options::RexData;
+
+//IMPLEMENTATIONS
+impl RexData
+{
+    //CREATE EMPTY RexData
+    pub fn empty() -> Self
+    {
+        Self
+        {
+            output: None,
+            key: None,
+        }
+    }
+}
