8d1ebdde2f991674f85e0a2d8280fdebb45f86c2
ENGO150 <engo@satan.red>
2025-09-28 18:48:19 +0200

    auto-update configuration files
    also replace toml with toml_edit

--- src/chat/config/mod.rs
+++ src/chat/config/mod.rs
@@ -23,6 +23,10 @@ use std::
     fs::{ self, File },
 };
 
+use reqwest::blocking::Response;
+
+use toml_edit::{ DocumentMut, Value };
+
 use crate::
 {
     core::misc,
@@ -33,6 +37,11 @@ use crate::
 fn config_path(filename: &str) -> String //GET CONFIGURATION PATH
 {
     misc::get_why2_dir() + filename
+}
+
+fn fetch_config(filename: &str) -> Response //FETCH CONFIG FROM GIT
+{
+    reqwest::blocking::get(options::CONFIG_URL.to_owned() + filename).expect("Failed to fetch config file")
 }
 
 fn init_config(filename: &str) //CREATE CONFIG IF MISSING
@@ -49,15 +58,49 @@ fn init_config(filename: &str) //CREATE CONFIG IF MISSING
     }
 }
 
-fn get_data(path: &str) -> toml::Value //GET Value FROM path
+fn get_data(path: &str) -> DocumentMut //GET DocumentMut FROM path
 {
     let content = fs::read_to_string(path).expect("Failed to read config"); //READ CONFIG FILE
-    toml::from_str(&content).expect("Failed to parse config") //PARSE CONFIG & RETURN
+    content.parse::<DocumentMut>().expect("Failed to parse config") //PARSE CONFIG & RETURN
 }
 
 fn config_read(filename: &str, key: &str) -> String //READ CONFIG
 {
-    get_data(&config_path(filename)).get(key).expect("Key not found").to_string().replace("\"", "").trim().to_string()
+    let data = get_data(&config_path(filename));
+
+    //READ
+    if let Some(value) = data.get(key) //FOUND IN CONFIG
+    {
+        //USE APPROPRIATE DATATYPE
+        return match value.as_value().expect("Invalid config")
+        {
+            Value::String(s) => s.value().to_string(),
+            Value::Integer(i) => i.value().to_string(),
+            Value::Boolean(b) => b.value().to_string(),
+
+            _ => panic!("Unsupported config datatype")
+        }
+    }
+
+    //key NOT FOUND IN CONFIG, FETCH CONFIG AND INSERT NEW KEY
+    let mut new_config: DocumentMut = fetch_config(filename).text().expect("Failed to fetch config file").parse().expect("Failed to parse config");
+
+    //LOAD OLD CONFIG
+    for (key, old_value) in data.as_table()
+    {
+        //NEW CONFIG CONTAINS SAME KEY AS THE OLD ONE, USE OLD VALUE
+        if let Some(item) = new_config.get_mut(key)
+        {
+            //COPY OLD VALUE
+            *item.as_value_mut().expect("Updating config failed") = old_value.as_value().expect("Invalid config").clone();
+        }
+    }
+
+    //UPDATE
+    fs::write(&config_path(&filename), new_config.to_string()).expect("Updating config file failed");
+
+    //REPEAT
+    config_read(filename, key)
 }
 
 //PUBLIC
@@ -101,10 +144,10 @@ pub fn server_users_write(key: &str, value: &str) //WRITE TO server_users.toml
     let mut data = get_data(&path);
 
     //WRITE
-    data.as_table_mut().expect("Writing to config failed").insert(key.to_string(), value.into());
+    data.as_table_mut().insert(key, value.into());
 
     //SAVE
-    fs::write(&path, toml::to_string(&data).expect("Parsing config failed")).expect("Saving config failed");
+    fs::write(&path, data.to_string()).expect("Saving config failed");
 }
 
 pub fn server_users_contains(key: &str) -> bool //CHECK IF server_users.toml contains
