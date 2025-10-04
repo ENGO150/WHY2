6d42f8ac45e03eeeef4becca54cc99b38f1673ba
ENGO150 <engo@satan.red>
2025-10-04 16:13:49 +0200

    prevent from incorrect feature usage
    add custom build script

---
+++ build.rs
@@ -0,0 +1,37 @@
+/*
+This is part of WHY2
+Copyright (C) 2022-2025 Václav Šmejkal
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+fn main()
+{
+    //ENSURE CORRECT FEATURE USAGE
+    let client_feature = std::env::var("CARGO_FEATURE_CLIENT").is_ok();
+    let server_feature = std::env::var("CARGO_FEATURE_SERVER").is_ok();
+    let chat_feature = std::env::var("CARGO_FEATURE_CHAT").is_ok();
+
+    //DIRECT CHAT FEATURE USE
+    if chat_feature && !(client_feature || server_feature)
+    {
+        panic!("Do not enable `chat` directly — use `client` or `server`.");
+    }
+
+    //USE OF SERVER AND CLIENT FEATURES COMBINED
+    if client_feature && server_feature
+    {
+        panic!("Features `client` and `server` cannot be enabled at the same time.");
+    }
+}
