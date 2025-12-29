312e18ab3bbf71ac4c7965afb51cb2cb00d3346a
ENGO150 <engo@satan.red>
2025-12-29 23:09:30 +0100

    implement receive section on client

--- chat/network/voice/client.rs
+++ chat/network/voice/client.rs
@@ -16,7 +16,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-use std::net::UdpSocket;
+use std::
+{
+    sync::Arc,
+    net::UdpSocket,
+};
 
 use cpal::
 {
@@ -35,7 +39,18 @@ use audiopus::
     Channels,
     SampleRate,
     Application,
-    coder::Encoder,
+    coder::{ Encoder, Decoder },
+};
+
+use ringbuf::
+{
+    HeapRb,
+    traits::
+    {
+        Split,
+        Producer,
+        Consumer,
+    },
 };
 
 use gag::Gag;
@@ -63,7 +78,7 @@ fn find_device(mut devices: impl Iterator<Item = Device>) -> Option<Device>
 pub fn listen_server_voice()
 {
     //CONNECT
-    let socket = UdpSocket::bind("0.0.0.0:0").expect("Binding UDP failed");
+    let socket = Arc::new(UdpSocket::bind("0.0.0.0:0").expect("Binding UDP failed"));
     socket.connect(chat_options::get_server_address()).expect("Connecting to server UDP failed");
 
     //INIT AUDIO HOST
@@ -76,13 +91,17 @@ pub fn listen_server_voice()
     let input_device = find_device(host.input_devices().expect("No input device found"))
         .or_else(|| host.default_input_device()).unwrap();
 
+    //FIND OUTPUT DEVICE
+    let output_device = find_device(host.output_devices().expect("No output device found"))
+        .or_else(|| host.default_output_device()).unwrap();
+
     //DISABLE SUPPRESSION
     drop(stderr_gag);
 
     //CONFIGURE CPAL INPUT
-    let mut input_config: StreamConfig = input_device.default_input_config().unwrap().into();
-    input_config.sample_rate = options::SAMPLE_RATE;
-    input_config.channels = 1;
+    let mut stream_config: StreamConfig = input_device.default_input_config().unwrap().into();
+    stream_config.sample_rate = options::SAMPLE_RATE;
+    stream_config.channels = 1;
 
     //PREPARE OPUS ENCODER
     let opus_encoder = Encoder::new
@@ -92,12 +111,19 @@ pub fn listen_server_voice()
         Application::Voip
     ).unwrap();
 
+    let mut opus_decoder = Decoder::new
+    (
+        SampleRate::Hz48000,
+        Channels::Mono,
+    ).unwrap();
+
     //INPUT BUFFERS
     let mut input_accum: Vec<f32> = Vec::with_capacity(options::FRAME_SIZE * 2);
     let mut encoded_buffer = [0u8; 1500]; //ALLOCATE BUFFER TO STANDARD MTU
 
     //CONFIGURE INPUT STREAM
-    let input_stream = input_device.build_input_stream(&input_config, move |data: &[f32], _: &_|
+    let send_socket = socket.clone();
+    let input_stream = input_device.build_input_stream(&stream_config, move |data: &[f32], _: &_|
     {
         //ACCUMULATE
         input_accum.extend_from_slice(data);
@@ -107,18 +133,49 @@ pub fn listen_server_voice()
         {
             let frame: Vec<f32> = input_accum.drain(0..options::FRAME_SIZE).collect();
 
-            //ENCODE
-            match opus_encoder.encode_float(&frame, &mut encoded_buffer)
+            //ENCODE (IGNORE ERRORS)
+            if let Ok(len) = opus_encoder.encode_float(&frame, &mut encoded_buffer)
             {
-                Ok(len) =>
-                {
-                    voice::send(&socket, &encoded_buffer[..len]).unwrap();
-                },
-                Err(_) => {}, //IGNORE ENCODER ERRORS
+                voice::send(&send_socket, &encoded_buffer[..len]).unwrap();
             }
         }
     }, |_| {}, None).unwrap();
 
-    input_stream.play().unwrap();
-    loop {}
+    //JITTER BUFFER
+    let rb = HeapRb::<f32>::new(options::FRAME_SIZE); //~400ms BUFFER
+    let (mut producer, mut consumer) = rb.split();
+
+    //CONFIGURE OUTPUT STREAM
+    let output_stream = output_device.build_output_stream(&stream_config, move |data: &mut [f32], _: &_|
+    {
+        let read_count = consumer.pop_slice(data);
+
+        //FILL WITH SILENCE ON UNDERRUN
+        if read_count < data.len() {
+            for i in read_count..data.len() {
+                data[i] = 0.0;
+            }
+        }
+    }, |_| {}, None).unwrap();
+
+    //RUN STREAMS
+    input_stream.play().unwrap();  //INPUT
+    output_stream.play().unwrap(); //OUTPUT
+
+    //OUTPUT BUFFERS
+    let mut network_buffer: Vec<u8>;
+    let mut decoded_buffer = [0.0f32; options::FRAME_SIZE];
+
+    loop
+    {
+        //READ
+        network_buffer = voice::receive(&socket);
+
+        //DECODE
+        if let Ok(decoded_len) = opus_decoder.decode_float(Some(&network_buffer), &mut decoded_buffer[..], false)
+        {
+            //PUSH TO RINGBUFFER
+            producer.push_slice(&decoded_buffer[..decoded_len]);
+        }
+    }
 }
