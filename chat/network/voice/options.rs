637d0116a61d8eabc7e7c4dcd2d3d930b2031c01
ENGO150 <engo@satan.red>
2026-01-07 18:50:24 +0100

    decrease SOUND_EFFECT_VOLUME to 10%
    i hope it will sound similar on other machines as my audio setup is fucked lol

--- chat/network/voice/options.rs
+++ chat/network/voice/options.rs
@@ -34,7 +34,7 @@ pub const TRESHOLD_CLOSE: f32       = 0.001;                                    
 pub const HOLD_FRAMES: usize        = 10;                                       //~200ms HOLD TIME
 pub const MIXING_TRESHOLD: f32      = 0.001;                                    //SPEAKER DETECTION NOISE TRESHOLD
 pub const ACTIVITY_TRESHOLD: usize  = 100;                                      //SERVER ACTIVITY TIMER RESET TRESHOLD (~2000ms)
-pub const SOUND_EFFECT_VOLUME: f32  = 0.2;                                      //VOLUME OF SOUND EFFECTS (20%)
+pub const SOUND_EFFECT_VOLUME: f32  = 0.1;                                      //VOLUME OF SOUND EFFECTS (10%)
 
 pub const ACTIVITY_HOLD: usize      = (SAMPLE_RATE / 10) as usize;              //HOW LONG AFTER SPEAKING CLIENT BECOMES INACTIVE (~100ms)
 pub const DISPLAY_HOLD: usize       = 200;                                      //ACTIVITY_HOLD BUT MS FOR DISPLAY WINDOW
