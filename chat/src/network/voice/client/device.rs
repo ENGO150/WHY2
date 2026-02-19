775acefba8307eb603d279289638506c7a2f5a7d
ENGO150 <engo@satan.red>
2026-02-19 18:40:11 +0100

    implement audio device selection
    this is some sexy shit boi

--- chat/src/network/voice/client/device.rs
+++ chat/src/network/voice/client/device.rs
@@ -16,6 +16,88 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-pub fn setup_devices()
+use std::io::{ self, Write };
+
+use cpal::
 {
+    Device,
+    traits::{ DeviceTrait, HostTrait },
+};
+
+use crate::config;
+
+//PRIVATE
+fn prompt_selection(devices: &[String], config: &str) -> usize
+{
+    //PROMPT
+    let mut input: String;
+    loop
+    {
+        print!("\nSelect device: ");
+        io::stdout().flush().unwrap();
+        input = String::new(); //CLEAR BUFFER
+        io::stdin().read_line(&mut input).unwrap(); //READ INPUT
+
+        if let Ok(idx) = input.trim().parse::<usize>() && idx != 0
+        {
+            if let Some(device) = devices.get(idx - 1)
+            {
+                config::client_write(config, device);
+                println!("Set to: {}", device);
+                return idx - 1;
+            }
+        }
+
+        println!("Invalid selection!");
+    }
+}
+
+fn load_devices<T>(all_devices: T) -> Vec<String>
+where
+    T: Iterator<Item = Device>
+{
+    //COLLECT
+    let mut devices = Vec::new();
+    for device in all_devices
+    {
+        //DO NOT PUSH DUPLICATES
+        if let Ok(name) = device.description().map(|d| d.to_string()) && !devices.contains(&name)
+        {
+            devices.push(name);
+        }
+    }
+
+    //SORT & RETURN
+    devices.sort();
+    devices
+}
+
+fn list_devices(devices: &[String])
+{
+    for (i, device) in devices.iter().enumerate()
+    {
+        println!("[{}]: {device}", i + 1);
+    }
+}
+
+//PUBLIC
+pub fn setup_devices() //SELECT AUDIO DEVICES AND STORE IN CLIENT CONFIG
+{
+    let host = cpal::default_host();
+
+    //COLLECT INPUT DEVICES
+    println!("Available input devices\n=======================");
+    let input_devices = load_devices(host.input_devices().unwrap());
+
+    //GET INPUT DEVICE
+    list_devices(&input_devices); //LIST
+    prompt_selection(&input_devices, "input_device"); //PROMPT
+
+    //COLLECT OUTPUT DEVICES
+    println!("\nAvailable output devices\n========================");
+    let output_devices = load_devices(host.output_devices().unwrap());
+
+    //GET OUTPUT DEVICE
+    list_devices(&output_devices); //LIST
+    prompt_selection(&output_devices, "output_device"); //PROMPT
 }
