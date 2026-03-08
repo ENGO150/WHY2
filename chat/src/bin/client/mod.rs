798b76feb1dafd1411b73c627c78f79328674d7e
ENGO150 <engo@satan.red>
2026-03-08 12:32:55 +0100

    revert "send empty packet to server on connection"
    turns out it's not WHY2's failure :)
    
    this reverts commit 777899388c08c754d8012deb1322430f007c16de

--- chat/src/bin/client/mod.rs
+++ chat/src/bin/client/mod.rs
@@ -556,7 +556,6 @@ fn main()
     let write_stream_listen = write_stream.clone();
 
     //LISTEN TO SERVER
-    network::send(&mut write_stream.lock().unwrap(), MessagePacket::default(), None); //WAKE SERVER
     thread::spawn(move || client::listen_server(&mut (&mut stream, write_stream_listen), tx));
 
     //ENABLE RAW MODE
