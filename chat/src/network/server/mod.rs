7afed479886aa7a7e3c200993275305c3571e829
ENGO150 <engo@satan.red>
2026-05-14 17:23:25 +0200

    add file sharing backend files

--- chat/src/network/server/mod.rs
+++ chat/src/network/server/mod.rs
@@ -16,6 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+//MODULES
+pub mod file;
+
 use std::
 {
     env,
