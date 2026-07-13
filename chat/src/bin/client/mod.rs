f70814d7d7ba8b47e92baf88270667781c17e4d9
ENGO150 <engo@satan.red>
2026-07-13 14:09:32 +0200

    move audio device setup to client binary

--- chat/src/bin/client/mod.rs
+++ chat/src/bin/client/mod.rs
@@ -78,11 +78,15 @@ use why2_chat::
     },
 };
 
+#[cfg(feature = "client_voice")]
+use cpal::
+{
+    Device,
+    traits::{ DeviceTrait, HostTrait },
+};
+
 #[cfg(feature = "client_screen")]
 use winit::event_loop::EventLoop;
-
-#[cfg(feature = "client_voice")]
-use why2_chat::network::voice::client::device;
 
 #[cfg(feature = "client_screen")]
 use why2_chat::network::screen::client::
@@ -450,6 +454,84 @@ fn get_colors() -> MessageColors //READ COLORS FROM CONFIG
     }
 }
 
+#[cfg(feature = "client_voice")]
+fn prompt_selection(devices: &[String], config: &str) -> usize
+{
+    //PROMPT
+    let mut input: String;
+    loop
+    {
+        print!("\nSelect device: ");
+        io::stdout().flush().unwrap();
+        input = String::new(); //CLEAR BUFFER
+        io::stdin().read_line(&mut input).unwrap(); //READ INPUT
+
+        if let Ok(idx) = input.trim().parse::<usize>() && idx != 0
+        {
+            if let Some(device) = devices.get(idx - 1)
+            {
+                config::client_write(config, device);
+                println!("Set to: {}", device);
+                return idx - 1;
+            }
+        }
+
+        println!("Invalid selection!");
+    }
+}
+
+#[cfg(feature = "client_voice")]
+fn load_devices<T>(all_devices: T) -> Vec<String>
+where
+    T: Iterator<Item = Device>
+{
+    //COLLECT
+    let mut devices = Vec::new();
+    for device in all_devices
+    {
+        //DO NOT PUSH DUPLICATES
+        if let Ok(name) = device.description().map(|d| d.to_string()) && !devices.contains(&name)
+        {
+            devices.push(name);
+        }
+    }
+
+    //SORT & RETURN
+    devices.sort();
+    devices
+}
+
+#[cfg(feature = "client_voice")]
+fn list_devices(devices: &[String])
+{
+    for (i, device) in devices.iter().enumerate()
+    {
+        println!("[{}]: {device}", i + 1);
+    }
+}
+
+#[cfg(feature = "client_voice")]
+fn setup_devices() //SELECT AUDIO DEVICES AND STORE IN CLIENT CONFIG
+{
+    let host = cpal::default_host();
+
+    //COLLECT INPUT DEVICES
+    println!("Available input devices\n=======================");
+    let input_devices = load_devices(host.input_devices().unwrap());
+
+    //GET INPUT DEVICE
+    list_devices(&input_devices); //LIST
+    prompt_selection(&input_devices, "input_device"); //PROMPT
+
+    //COLLECT OUTPUT DEVICES
+    println!("\nAvailable output devices\n========================");
+    let output_devices = load_devices(host.output_devices().unwrap());
+
+    //GET OUTPUT DEVICE
+    list_devices(&output_devices); //LIST
+    prompt_selection(&output_devices, "output_device"); //PROMPT
+}
+
 fn main()
 {
     //CREATE CHANNEL
@@ -478,7 +560,7 @@ fn main()
         } else if arg == "--audio-setup" && env::args().len() == 2
         {
             #[cfg(feature = "client_voice")]
-            device::setup_devices();
+            setup_devices();
         } else if arg == "--help" && env::args().len() == 2
         {
             println!
--- chat/src/network/voice/client/mod.rs
+++ chat/src/network/voice/client/mod.rs
@@ -17,7 +17,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 //MODULES
-pub mod device;
 pub mod sfx;
 pub mod options;
 
